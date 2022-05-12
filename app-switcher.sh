61253687f4a10fbc3d2a97b8ed7481401148adc0
Patrick Salmers <patrick.salmers@shopify.com>
2022-05-12 16:24:33 -0400

    adds karabiner updates with app switching script

---
+++ app-switcher.sh
@@ -0,0 +1,31 @@
+osascript -l JavaScript << EndOfScript
+    var appChoice = [
+            undefined,
+            "Emacs",
+            "Chrome",
+            "iTerm",
+            "VSCode",
+            "Slack",
+            "Chrome meet.google",
+            "Chrome mail.google",
+        ][$1].split(" ")
+    console.log("app choice " + appChoice)
+    var appName = appChoice[0]
+    var appTab = appChoice[1]
+
+    if (!appName) throw "No app for hotkey $1";
+
+    var app = Application(appName)
+
+    app.activate();
+
+    if(appTab) {
+        for (win of app.windows()) {
+            var tabIndex = win.tabs().findIndex(tab => tab.url().match(new RegExp(appTab)));
+            if (tabIndex != -1) {
+                win.activeTabIndex = (tabIndex + 1);
+                win.index = 1;
+            }
+        }
+    }
+EndOfScript
