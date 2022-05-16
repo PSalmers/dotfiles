0be91c918971ddef5df8f99d29cb0bd9870bc26d
Patrick Salmers <patrick.salmers@shopify.com>
2022-05-16 12:16:56 -0400

    test spin config

---
+++ install.sh
@@ -0,0 +1,16 @@
+ln -sf ~/dotfiles/editors/init.vim ~/.config/nvim/init.vim
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+
+if !command -v rg &> /dev/null; then
+    sudo apt-get install -y ripgrep
+fi
+
+if !command -v fzf &> /dev/null; then
+    sudo apt-get install -y fzf
+fi
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
