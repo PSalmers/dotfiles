d3c083958e7654cd915da7a4aac71401ec7eb28a
Patrick Salmers <patrick.salmers@shopify.com>
2023-01-19 11:13:46 -0500

    Updated Emacs config, streamlined install.sh
    Removed a lot of terminal configuration as unnecessary. Added lines to
    upgrade to Emacs28, so that my tty config can match my local config.
    Also added code for working in tsx.

--- editors/tty-dev.el
+++ editors/tty-dev.el
@@ -1,5 +1,6 @@
-(setq make-backup-files nil)
-(setq auto-save-default nil)
+(setq make-backup-files nil
+      auto-save-default nil)
+
 ;; Text editing tweaks
 (global-set-key (kbd "M-z") 'zap-up-to-char)
 (global-set-key (kbd "C-.") 'repeat)
@@ -25,19 +26,6 @@
 (use-package counsel
   :config
   (counsel-mode))
-(use-package projectile
-  :config
-  (projectile-mode +1)
-  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
-(use-package rg
-  :config
-  (rg-enable-default-bindings))
-(use-package eglot
-;; Gems: Add to bashrc/zshrc to add gems to path
-;; if which ruby >/dev/null && which gem >/dev/null; then
-;;    PATH="$(ruby -r rubygems -e 'puts Gem.user_dir')/bin:$PATH"
-;; fi
-)
 (use-package which-key
   :config
   (which-key-mode))
@@ -58,3 +46,52 @@
 
 ;; Keeps customizations from polluting my init
 (setq custom-file (concat user-emacs-directory "/custom.el"))
+
+;; Tsx from https://vxlabs.com/2022/06/12/typescript-development-with-emacs-tree-sitter-and-lsp-in-2022/
+(use-package tree-sitter
+  :ensure t
+  :config
+  ;; activate tree-sitter on any buffer containing code for which it has a parser available
+  (global-tree-sitter-mode)
+  ;; you can easily see the difference tree-sitter-hl-mode makes for python, ts or tsx
+  ;; by switching on and off
+  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
+
+(use-package tree-sitter-langs
+  :ensure t
+  :after tree-sitter)
+
+(use-package typescript-mode
+  :after tree-sitter
+  :config
+  ;; we choose this instead of tsx-mode so that eglot can automatically figure out language for server
+  ;; see https://github.com/joaotavora/eglot/issues/624 and https://github.com/joaotavora/eglot#handling-quirky-servers
+  (define-derived-mode typescriptreact-mode typescript-mode
+    "TypeScript TSX")
+  (setq typescript-indent-level 2)
+
+  ;; use our derived mode for tsx files
+  (add-to-list 'auto-mode-alist '("\\.tsx?\\'" . typescriptreact-mode))
+  ;; by default, typescript-mode is mapped to the treesitter typescript parser
+  ;; use our derived mode to map both .tsx AND .ts -> typescriptreact-mode -> treesitter tsx
+  (add-to-list 'tree-sitter-major-mode-language-alist '(typescriptreact-mode . tsx)))
+
+;; https://github.com/orzechowskid/tsi.el/
+;; great tree-sitter-based indentation for typescript/tsx, css, json
+(use-package tsi
+  :after tree-sitter
+  :quelpa (tsi :fetcher github :repo "orzechowskid/tsi.el")
+  ;; define autoload definitions which when actually invoked will cause package to be loaded
+  :commands (tsi-typescript-mode tsi-json-mode tsi-css-mode)
+  :init
+  (add-hook 'typescript-mode-hook (lambda () (tsi-typescript-mode 1)))
+  (add-hook 'json-mode-hook (lambda () (tsi-json-mode 1)))
+  (add-hook 'css-mode-hook (lambda () (tsi-css-mode 1)))
+  (add-hook 'scss-mode-hook (lambda () (tsi-scss-mode 1))))
+
+;; auto-format different source code files extremely intelligently
+;; https://github.com/radian-software/apheleia
+(use-package apheleia
+  :ensure t
+  :config
+  (apheleia-global-mode +1))
--- install.sh
+++ install.sh
@@ -1,32 +1,11 @@
-sudo apt-get install -y ripgrep
-sudo apt-get install -y zoxide
-sudo apt-get install -y fzf
-sudo apt-get install -y universal-ctags
-sudo apt-get install -y fd-find
-
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
- 
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-# Vim Setup
-
-# sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-#       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-# mkdir ~/.config/nvim/
-# ln -sf ~/dotfiles/editors/init.vim ~/.config/nvim/init.vim
-# nvim +PlugInstall +qall
-
 # Emacs setup
 mkdir ~/.emacs.d/
 ln -sf ~/dotfiles/editors/tty-dev.el ~/.emacs.d/init.el
+sudo apt remove --autoremove emacs emacs-common
+sudo add-apt-repository ppa:kelleyk/emacs
+sudo apt update
+sudo apt install emacs28-nox
 
 # Tmux
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 
-# I link after in case installs create any configs I wish to overwrite
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
- 
-# Dirty checking slows down the prompt on large repos, so I disable it
-git config --global --add oh-my-zsh.hide-dirty 1
-git config --global --add oh-my-zsh.hide-status 1
