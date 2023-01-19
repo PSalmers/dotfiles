c290f5df0da08897508b19e91f3b031306711b98
Patrick Salmers <patrick.salmers@shopify.com>
2023-01-19 14:17:45 -0500

    adds lsp installs

--- install.sh
+++ install.sh
@@ -6,7 +6,11 @@ sudo add-apt-repository -y ppa:kelleyk/emacs
 sudo apt-get -y update
 sudo apt-get -y install emacs28-nox
 
+nvm install -y node
+npm install -g typescript typescript-language-server typescript-eslint-language-service prettier
+gem install --user-install solargraph
 # Tmux
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+
 ln -sf ~/dotfiles/.zshrc ~/spin_zshrc
 
